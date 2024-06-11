4b72daa37e3383a1bf557cbb4ddd17af177ec1a9
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-11 13:04:19 +0200

    Implemented view details functionality

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react"
+
+
 const Blog = ({blog}) => {
+
+    const [showDetails, setShowDetails] = useState(false)
+
+    const clickHandler = () => {
+        setShowDetails(!showDetails)
+    }
+
+    const displayDetails = () => {
+        return(
+            <>
+                <p>{blog.url}</p>
+                <p>{blog.likes} <button>Like</button></p>
+                <p>{blog.author}</p>
+            </>
+        )
+    }
+
     return(
-    <>
-    <p>{blog.title} {blog.author}</p>
-    </>
+    <div className="blogpost">
+        <p>{blog.title} {blog.author} <button onClick={clickHandler}>{showDetails ? "hide" : "view"}</button></p>
+        {showDetails && displayDetails()}
+    </div>
     )
 }
 
