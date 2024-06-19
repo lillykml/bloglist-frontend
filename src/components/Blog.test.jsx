73333f682e00daae52bf065c7a192601df5d0ff4
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-19 14:44:32 +0200

    Fixed tests & 5.15

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -7,7 +7,8 @@ const blog = {
   title: 'Test Blog for Me',
   author: 'Benjamin Blümchen',
   url: 'https://fullstackopen.com/en/part5/testing_react_apps#exercises-5-13-5-16',
-  likes: 5
+  likes: 5,
+  user: "jdfö"
 }
 
 describe('<Blog />', async () => {
@@ -25,14 +26,28 @@ describe('<Blog />', async () => {
     expect(author).toBeDefined()
   })
 
-  test('Url and likes are display after expanding', async() => {
-    const container = render(<Blog blog={blog} />).container
+  test('Url and likes are display after expanding', async () => {
+    render(<Blog blog={blog} />)
     const user = userEvent.setup()
-    const button = container.querySelector('#blog-view-button')
-    user.click(button)
-    const likes = container.querySelector('#blog-likes')
-    const url = container.querySelector('#blog-url')
+    const button = screen.getByText('view', { exact: false })
+    await user.click(button)
+    const likes = screen.getByText(5)
+    const url = screen.getByText('https://fullstackopen.com/en/part5/testing_react_apps#exercises-5-13-5-16')
     expect(likes).toBeDefined()
     expect(url).toBeDefined()
   })
+
+  test('Records correct number of like clicks', async() => {
+    const mockHandler = vi.fn()
+    render(<Blog blog={blog} like={mockHandler}/>)
+    const user = userEvent.setup()
+    const viewbutton = screen.getByText('view')
+    await user.click(viewbutton)
+    const likebutton = screen.getByText('Like')
+    await user.click(likebutton)
+    await user.click(likebutton)
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
+
+
 })
